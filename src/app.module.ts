f10e15a4d60849bbf236d48286295f98514bc672
ArthurMN <arthurphb@alu.ufc.br>
2026-06-17 12:14:27 -0300

    feat: adiciona oportunidades de vendas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ContactModule } from './contact/contact.module';
+import { OpportunityModule } from './opportunity/opportunity.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ContactModule],
+  imports: [AuthModule, UserModule, ContactModule, OpportunityModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
